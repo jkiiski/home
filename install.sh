bf93642a486457f4e0c8f3a344d185d40a99596a
Janne Kiiski <janne.kiiski@gmail.com>
2013-01-29 00:44:40 +0200

    add configs

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# install apps
+
+sudo apt-get install zsh vim git ctags curl rake xfonts-terminus
+
+# setup shell
+
+# gnome-terminal solarized colors
+git clone https://github.com/sigurdga/gnome-terminal-colors-solarized.git
+gnome-terminal-colors-solarized/set_dark.sh
+rm -rf gnome-terminal-colors-solarized
+# oh-my-zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+# janus
+curl -Lo- https://bit.ly/janus-bootstrap | bash
+
+# copy configs...
+
+CONFIGS="\
+	.gitconfig \
+	.vimrc.after \
+	.zshrc"
+
+for c in $CONFIGS; do
+	cp -i $c $HOME/
+done
+
